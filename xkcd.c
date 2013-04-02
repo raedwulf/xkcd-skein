2e95e69c037c314475d9aaf2e5fc1cfbc27dedca
Michael Walker <mike@barrucadu.co.uk>
2013-04-02 20:06:16 +0100

    Tidy up code a bit

--- xkcd.c
+++ xkcd.c
@@ -1,7 +1,7 @@
 /*
  * Compile with gcc -O3 -std=c99 *.c -lcurl -lpthread -DSHIFT_CHAR -o xkcd-skein
 */
- 
+
 #include <time.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -14,7 +14,7 @@
 #include <signal.h>
 #include <assert.h>
 #include "skein.h"
- 
+
 /* The number of bits to use for the Skein 1024 hash */
 #define SKEIN_BITS      1024
 /* The length of the generated hash in bytes */
@@ -37,26 +37,28 @@
 #define COUNT_GRANULARITY (1 << 16)
 /* The timer update */
 #define TIMER_INTERVAL  30
- 
+
 size_t best = 424;
 size_t count = 0;
 size_t elapsed = 0;
 pthread_mutex_t best_mutex = PTHREAD_MUTEX_INITIALIZER;
- 
+
 FILE *logfile;
- 
+
 /* The xkcd-provided hash */
-const char* xkcdhashstr = "5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6";
-const uint8_t* xkcdhash =
-	"\x5b\x4d\xa9\x5f\x5f\xa0\x82\x80\xfc\x98\x79\xdf\x44\xf4\x18\xc8"
-	"\xf9\xf1\x2b\xa4\x24\xb7\x75\x7d\xe0\x2b\xbd\xfb\xae\x0d\x4c\x4f"
-	"\xdf\x93\x17\xc8\x0c\xc5\xfe\x04\xc6\x42\x90\x73\x46\x6c\xf2\x97"
-	"\x06\xb8\xc2\x59\x99\xdd\xd2\xf6\x54\x0d\x44\x75\xcc\x97\x7b\x87"
-	"\xf4\x75\x7b\xe0\x23\xf1\x9b\x8f\x40\x35\xd7\x72\x28\x86\xb7\x88"
-	"\x69\x82\x6d\xe9\x16\xa7\x9c\xf9\xc9\x4c\xc7\x9c\xd4\x34\x7d\x24"
-	"\xb5\x67\xaa\x3e\x23\x90\xa5\x73\xa3\x73\xa4\x8a\x5e\x67\x66\x40"
-	"\xc7\x9c\xc7\x01\x97\xe1\xc5\xe7\xf9\x02\xfb\x53\xca\x18\x58\xb6";
- 
+const uint8_t xkcdhash[] =
+  {0x5b, 0x4d, 0xa9, 0x5f, 0x5f, 0xa0, 0x82, 0x80, 0xfc, 0x98, 0x79, 0xdf,
+   0x44, 0xf4, 0x18, 0xc8, 0xf9, 0xf1, 0x2b, 0xa4, 0x24, 0xb7, 0x75, 0x7d,
+   0xe0, 0x2b, 0xbd, 0xfb, 0xae, 0x0d, 0x4c, 0x4f, 0xdf, 0x93, 0x17, 0xc8,
+   0x0c, 0xc5, 0xfe, 0x04, 0xc6, 0x42, 0x90, 0x73, 0x46, 0x6c, 0xf2, 0x97,
+   0x06, 0xb8, 0xc2, 0x59, 0x99, 0xdd, 0xd2, 0xf6, 0x54, 0x0d, 0x44, 0x75,
+   0xcc, 0x97, 0x7b, 0x87, 0xf4, 0x75, 0x7b, 0xe0, 0x23, 0xf1, 0x9b, 0x8f,
+   0x40, 0x35, 0xd7, 0x72, 0x28, 0x86, 0xb7, 0x88, 0x69, 0x82, 0x6d, 0xe9,
+   0x16, 0xa7, 0x9c, 0xf9, 0xc9, 0x4c, 0xc7, 0x9c, 0xd4, 0x34, 0x7d, 0x24,
+   0xb5, 0x67, 0xaa, 0x3e, 0x23, 0x90, 0xa5, 0x73, 0xa3, 0x73, 0xa4, 0x8a,
+   0x5e, 0x67, 0x66, 0x40, 0xc7, 0x9c, 0xc7, 0x01, 0x97, 0xe1, 0xc5, 0xe7,
+   0xf9, 0x02, 0xfb, 0x53, 0xca, 0x18, 0x58, 0xb6};
+
 /**
  * Generate a random number in a given range (inclusive)
  */
@@ -64,7 +66,7 @@ static inline size_t randrange(size_t min, size_t max)
 {
 	return (rand() % (1 + max - min)) + min;
 }
- 
+
 /**
  * Generate a random string of the given length
  */
@@ -72,10 +74,10 @@ void randbytes(char *out, size_t len)
 {
 	for(size_t i = 0; i < len; i++)
 		out[i] = ALLOWED[randrange(0, ALLOWED_LEN)];
- 
+
 	out[len] = '\0';
 }
- 
+
 /**
  * Shift char
  */
@@ -84,37 +86,18 @@ static inline void shiftchar(char *out, size_t len)
 	size_t i = randrange(0, len - 1);
 	out[i] = ALLOWED[randrange(0, ALLOWED_LEN)];
 }
- 
+
 /**
  * Calculate the difference between the two strings
  */
 static inline size_t distance(const uint8_t* str1, const uint8_t* str2)
 {
+	size_t score = 0;
+
 #if SKEIN_HASH_SIZE == 128
-	size_t score = 0;
-       /* uint64_t *a = (uint64_t *)str1;*/
-	/*uint64_t *b = (uint64_t *)str2;*/
- 
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
-	/*score += __builtin_popcountll(*a++ ^ *b++);*/
- 
-        uint32_t *a = (uint32_t *)str1;
+    uint32_t *a = (uint32_t *)str1;
 	uint32_t *b = (uint32_t *)str2;
- 
+
 	score += __builtin_popcountl(*a++ ^ *b++);
 	score += __builtin_popcountl(*a++ ^ *b++);
 	score += __builtin_popcountl(*a++ ^ *b++);
@@ -147,40 +130,14 @@ static inline size_t distance(const uint8_t* str1, const uint8_t* str2)
 	score += __builtin_popcountl(*a++ ^ *b++);
 	score += __builtin_popcountl(*a++ ^ *b++);
 	score += __builtin_popcountl(*a++ ^ *b++);
- 
-	return score;
 #else
-	return distance_old(str1, str2);
-#endif
-}
- 
-/**
- * Calculate the difference between the two strings
- */
-static inline size_t distance_old(const uint8_t* str1, const uint8_t* str2)
-{
-	size_t score = 0;
-	char bits = 0;
- 
 	for(size_t i = 0; i < SKEIN_HASH_SIZE; i++)
 		score += __builtin_popcount(str1[i] ^ str2[i]);
- 
+#endif
+
 	return score;
 }
- 
-/**
- * Convert a string of bytes into a hexadecimal string
- */
-void to_hex(const uint8_t* str, char *out, size_t len)
-{
-	char* p = out;
- 
-	for(size_t i = 0; i < len; i++)
-		p += sprintf(p, "%.2x", str[i]);
- 
-	*p = '\0';
-}
- 
+
 /**
  * Calculate the Skein 1024 hash of a string
  */
@@ -193,14 +150,15 @@ static inline void hash(const char* str, size_t len, size_t bytes,
 	Skein1024_Update(&ctx, (uint8_t *)str, len);
 	Skein1024_Final(&ctx, skeinhash);
 }
- 
- 
-/* Discard the curl output */
+
+/**
+ * Discard the curl output
+ */
 size_t curl_devnull(char *ptr, size_t size, size_t nmemb, void *userdata)
 {
 	return size * nmemb;
 }
- 
+
 /**
  * Submit a string to xkcd
  */
@@ -209,21 +167,41 @@ void submit_to_xkcd(const char* str)
 	CURL *curl;
 	CURLcode res;
 	char postdata[HASH_SIZE + 11] = "hashable=";
- 
+
 	strcat(postdata, (char*)str);
- 
+
 	curl = curl_easy_init();
 	if(curl) {
 		curl_easy_setopt(curl, CURLOPT_URL, "http://almamater.xkcd.com/?edu=" INSTITUTION);
 		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postdata);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_devnull);
- 
+
 		res = curl_easy_perform(curl);
- 
+
 		curl_easy_cleanup(curl);
 	}
 }
- 
+
+/**
+ * Log a new best score and submit it to xkcd
+ */
+static inline void new_best_score(int threadid, size_t len, char* str, size_t mybest, size_t score)
+{
+  char str1[STR_MAX+1];
+  strncpy(str1, str, len);
+  str1[len] = '\0';
+
+  printf("[%zu] New best string(%zu): %s (old: %zu) (new: %zu)\n", threadid, len, str1, mybest, score);
+
+  fprintf(logfile, "[%zu] New best string(%zu): %s (old: %zu) (new: %zu)\n", threadid, len, str1, mybest, score);
+  fflush(logfile);
+
+  submit_to_xkcd(str);
+}
+
+/**
+ * Start up a hash cracking thread
+ */
 void* threaded_solver(void* data)
 {
 	size_t threadid = (size_t)data;
@@ -231,57 +209,50 @@ void* threaded_solver(void* data)
 	char str[STR_MAX + 1];
 	size_t mybest = best;
 	size_t mycount = 0;
- 
+
 	printf("Thread %zu started.\n", threadid);
-	/*size_t len = randrange(STR_MIN, STR_MAX);*/
+
 #ifdef SHIFT_CHAR
 	size_t len = STR_MAX;
 	randbytes(str, len);
 #endif
- 
+
 	while(true) {
 		size_t len = randrange(STR_MIN, STR_MAX);
+
 #ifdef SHIFT_CHAR
 		shiftchar(str, len);
 #else
 		randbytes(str, len);
 #endif
- 
+
 		hash(str, len, SKEIN_BITS, skeinhash);
 		size_t score = distance(skeinhash, xkcdhash);
-		/*assert(score == distance_old(skeinhash, xkcdhash));*/
 		mycount++;
- 
+
 		if (!(mycount & (COUNT_GRANULARITY - 1)))
-			__sync_fetch_and_add(&count, COUNT_GRANULARITY);
+          __sync_fetch_and_add(&count, COUNT_GRANULARITY);
+
 #if defined(__APPLE__) && !defined(__GNUC__)
 		if (mybest > score) {
-			mybest = score;
-			if (best > mybest) {
-				pthread_mutex_lock(&best_mutex);
-				if (best > mybest) {
-					best = mybest;
-					char str1[STR_MAX+1]; strncpy(str1, str, len); str1[len] = '\0';
-					printf("[%zu] New best string(%zu): %s (old: %zu) (new: %zu)\n", threadid, len, str1, mybest, score);
-					fprintf(logfile, "[%zu] New best string(%zu): %s (old: %zu) (new: %zu)\n", threadid, len, str1, mybest, score);
-					fflush(logfile);
-					submit_to_xkcd(str);
-				}
-				pthread_mutex_unlock(&best_mutex);
-			}
+          mybest = score;
+          if (best > mybest) {
+            pthread_mutex_lock(&best_mutex);
+            if (best > mybest) {
+              best = mybest;
+              new_best_score(threadid, len, str, mybest, score);
+            }
+            pthread_mutex_unlock(&best_mutex);
+          }
 		}
 #else
 		while (mybest > score) {
-			if (__sync_bool_compare_and_swap(&best, mybest, score)) {
-				char str1[STR_MAX+1]; strncpy(str1, str, len); str1[len] = '\0';
-				printf("[%zu] New best string(%zu): %s (old: %zu) (new: %zu)\n", threadid, len, str1, mybest, score);
-				fprintf(logfile, "[%zu] New best string(%zu): %s (old: %zu) (new: %zu)\n", threadid, len, str1, mybest, score);
-				fflush(logfile);
-				mybest = score;
-				submit_to_xkcd(str);
-			} else {
-				mybest = best;
-			}
+          if (__sync_bool_compare_and_swap(&best, mybest, score)) {
+            new_best_score(threadid, len, str, mybest, score);
+            mybest = score;
+          } else {
+            mybest = best;
+          }
 		}
 #endif
 	}
@@ -290,12 +261,12 @@ void* threaded_solver(void* data)
 static inline void set_timer()
 {
 	struct itimerval tout_val;
- 
+
 	tout_val.it_interval.tv_sec = 0;
 	tout_val.it_interval.tv_usec = 0;
 	tout_val.it_value.tv_sec = TIMER_INTERVAL;
 	tout_val.it_value.tv_usec = 0;
- 
+
 	setitimer(ITIMER_REAL, &tout_val,0);
 }
  
@@ -303,50 +274,51 @@ void alarm_benchmark(int i)
 {
 	signal(SIGALRM, alarm_benchmark);
 	elapsed += TIMER_INTERVAL;
- 
+
 	printf("Count: %zu, Elapsed: %zu, Speed: %g hash/sec\n",
 		count, elapsed,
 		(double)count / (double)elapsed);
+
 	fprintf(logfile, "Count: %zu, Elapsed: %zu, Speed: %g hash/sec\n",
 		count, elapsed,
 		(double)count / (double)elapsed);
- 
+
 	set_timer();
 }
- 
+
 int main(void)
 {
 	srand((unsigned int) time(NULL));
 	curl_global_init(CURL_GLOBAL_ALL);
- 
+
 	pthread_t threads[NUM_THREADS];
- 
+
 	// Tell of the mode.
 #ifdef SHIFT_CHAR
 	printf("Shifting characters since 2013.\n");
 #else
 	printf("Being random since 2013.\n");
 #endif
- 
+
 	// Log file.
 	logfile = fopen("output.log", "a");
- 
+
 	// Start the benchmarking.
 	set_timer();
 	signal(SIGALRM, alarm_benchmark);
- 
+
 	// Fire off the threads.
 	if (NUM_THREADS == 1) {
-		threaded_solver(NULL);
+      threaded_solver(NULL);
 	} else {
-		for(size_t i = 0; i < NUM_THREADS; i++)
-			pthread_create(&(threads[i]), NULL, threaded_solver, (void*) i);
- 
-		for(size_t i = 0; i < NUM_THREADS; i++)
-			pthread_join(threads[i], NULL);
+      for(size_t i = 0; i < NUM_THREADS; i++)
+        pthread_create(&(threads[i]), NULL, threaded_solver, (void*) i);
+
+      for(size_t i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i], NULL);
 	}
- 
+
 	fclose(logfile);
- 
+
 	curl_global_cleanup();
 }
